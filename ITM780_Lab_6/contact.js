ed7791ac47e50823ead4ef344bb31ba39dfb22ab
thomasle8 <thomas.le@torontomu.ca>
2024-10-30 12:26:01 -0400

    Update contact.js

--- ITM780_Lab_6/contact.js
+++ ITM780_Lab_6/contact.js
@@ -13,20 +13,17 @@ function submit() {
     var dob = document.getElementById('dob').value;
 
     // Store form input values to local storage
-    var fi = localStorage.setItem("firstName", firstName);
-    var la = localStorage.setItem("lastName", lastName);
-    var em = localStorage.setItem("email", email);
-    var ph = localStorage.setItem("phone", phone);
-    var me = localStorage.setItem("message", message);
-    var inq = localStorage.setItem("inquiryType", inquiryType);
-    var co = localStorage.setItem("contactMethod", contactMethod);
-    var cou = localStorage.setItem("country", country);
-    var ag = localStorage.setItem("age", age);
-    var dob2 = localStorage.setItem("dob", dob);
+    localStorage.setItem("firstName", firstName);
+    localStorage.setItem("lastName", lastName);
+    localStorage.setItem("email", email);
+    localStorage.setItem("phone", phone);
+    localStorage.setItem("message", message);
+    localStorage.setItem("inquiryType", JSON.stringify(inquiryType)); // Store as JSON string
+    localStorage.setItem("contactMethod", contactMethod);
+    localStorage.setItem("country", country);
+    localStorage.setItem("age", age);
+    localStorage.setItem("dob", dob);
 
-
-
-
-
-
+    // Confirmation message
+    alert("Your data has been saved to local storage!");
 }
